816ecfcca1423e6821d3223859f8d1a11296c609
Heather <larsen.hv@gmail.com>
2022-11-07 13:31:30 -0800

    Rename main menu view and controller to main form

--- src/main/java/warga/inventorysystem/HelloApplication.java
+++ src/main/java/warga/inventorysystem/HelloApplication.java
@@ -11,7 +11,7 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/warga/inventorysystem/view/MainMenu.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/warga/inventorysystem/view/MainForm.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 600, 400);
         stage.setTitle("Hello!");
         stage.setScene(scene);
