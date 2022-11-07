3590510480b630432bf9ddf8eeb533c02739b830
Heather <larsen.hv@gmail.com>
2022-11-07 09:26:41 -0800

    Create class Inventory with getters and setters

---
+++ src/main/java/model/Inventory.java
@@ -0,0 +1,18 @@
+package model;
+
+import javafx.collections.ObservableList;
+
+public class Inventory {
+
+    private ObservableList<Part> allParts;
+
+    private ObservableList<Product> allProducts;
+
+    public ObservableList<Part> getAllParts() {
+        return allParts;
+    }
+
+    public ObservableList<Product> getAllProducts() {
+        return allProducts;
+    }
+}
