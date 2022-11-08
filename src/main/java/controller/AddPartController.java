e1b1201a243d1892a4dd9bb741224f3eda72331c
Heather <larsen.hv@gmail.com>
2022-11-08 11:50:23 -0800

    Add fx:id for GUI components and OnAction methods to controller

--- src/main/java/controller/AddPartController.java
+++ src/main/java/controller/AddPartController.java
@@ -2,23 +2,31 @@ package controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class AddPartController implements Initializable {
 
-    public Label TheLabel;
-    public int count = 0;
+
+    public ToggleGroup addPartTg;
+    public TextField addPartIdTxt;
+    public TextField addPartNameTxt;
+    public TextField addPartInvTxt;
+    public TextField addPartMaxTxt;
+    public TextField addPartMachineIdTxt;
+    public TextField addPartMinTxt;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         System.out.println("I am initialized");
     }
 
-    public void OnButtonClicked(ActionEvent actionEvent) {
-        System.out.println("I am clicked");
-        TheLabel.setText("You clicked the button: " + count++);
+    public void onActionAddPartSaveBtn(ActionEvent actionEvent) {
+    }
+
+    public void onActionAddPartCancelBtn(ActionEvent actionEvent) {
     }
 }
