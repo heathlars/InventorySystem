def331c8c819e796310b3924355aa1acfde79f4d
Heather <larsen.hv@gmail.com>
2022-11-08 11:44:01 -0800

    Add fx:id for GUI components and OnAction methods to controller

--- src/main/java/controller/MainFormController.java
+++ src/main/java/controller/MainFormController.java
@@ -2,23 +2,52 @@ package controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class MainFormController implements Initializable {
 
-    public Label TheLabel;
-    public int count = 0;
+
+    public TextField mainFormPartSearchTxtFld;
+    public TableView mainFormPartTbl;
+    public TableColumn mainFormPartIdCol;
+    public TableColumn mainFormPartNameCol;
+    public TableColumn mainFormPartInvCol;
+    public TableColumn mainFormPartPriceCol;
+    public TextField mainFormProductSearchTxtFld;
+    public TableView mainFormProductTbl;
+    public TableColumn mainFormProductIdCol;
+    public TableColumn mainFormProductNameCol;
+    public TableColumn mainFormProductInvCol;
+    public TableColumn mainFormProductPriceCol;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         System.out.println("I am initialized");
     }
 
-    public void OnButtonClicked(ActionEvent actionEvent) {
-        System.out.println("I am clicked");
-        TheLabel.setText("You clicked the button: " + count++);
+    public void onActionMainFormAddPart(ActionEvent actionEvent) {
+    }
+
+    public void onActionMainFormModifyPart(ActionEvent actionEvent) {
+    }
+
+    public void onActionMainFormDeletePart(ActionEvent actionEvent) {
+    }
+
+    public void onActionMainFormAddProduct(ActionEvent actionEvent) {
+    }
+
+    public void onActionMainFormModifyProduct(ActionEvent actionEvent) {
+    }
+
+    public void onActionMainFormDeleteProduct(ActionEvent actionEvent) {
+    }
+
+    public void onActionExitMainForm(ActionEvent actionEvent) {
     }
 }
