d4507d53b81bd61c5c904dfa1b3cd481f7a50e83
Heather <larsen.hv@gmail.com>
2022-11-08 12:14:20 -0800

    Add fx:id and OnAction methods for GUI components to controller

--- src/main/java/controller/ModifyProductController.java
+++ src/main/java/controller/ModifyProductController.java
@@ -2,23 +2,48 @@ package controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class ModifyProductController implements Initializable {
 
-    public Label TheLabel;
-    public int count = 0;
+
+    public TextField modifyProductIdTxt;
+    public TextField modifyProductNameTxt;
+    public TextField modifyProductInvTxt;
+    public TextField modifyProductPriceTxt;
+    public TextField modifyProductMaxTxt;
+    public TextField modifyProductMinTxt;
+    public TextField modifyProductSearchTxtFld;
+    public TableView modifyProductTopTbl;
+    public TableColumn modifyProductTopPartIdCol;
+    public TableColumn modifyProductTopPartNameCol;
+    public TableColumn modifyProductTopInvCol;
+    public TableColumn modifyProductTopPriceCol;
+    public TableView modifyProductBottomTbl;
+    public TableColumn modifyProductBottomPartIdCol;
+    public TableColumn modifyProductBottomPartNameCol;
+    public TableColumn modifyProductBottomInvCol;
+    public TableColumn modifyProductBottomPriceCol;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         System.out.println("I am initialized");
     }
 
-    public void OnButtonClicked(ActionEvent actionEvent) {
-        System.out.println("I am clicked");
-        TheLabel.setText("You clicked the button: " + count++);
+    public void onActionModifyProductAddBtn(ActionEvent actionEvent) {
+    }
+
+    public void onActionModifyProductRemovePartBtn(ActionEvent actionEvent) {
+    }
+
+    public void onActionModifyProductSaveBtn(ActionEvent actionEvent) {
+    }
+
+    public void onActionModifyProductCancelBtn(ActionEvent actionEvent) {
     }
 }
