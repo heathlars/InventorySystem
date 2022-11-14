802692b0e57bcc0e8e634c5f58066b37214d6317
Heather <larsen.hv@gmail.com>
2022-11-14 13:59:40 -0800

    Remove try/catch for save button

--- src/main/java/controller/AddPartController.java
+++ src/main/java/controller/AddPartController.java
@@ -5,7 +5,6 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
@@ -37,33 +36,10 @@ public class AddPartController implements Initializable {
     }
 
     public void onActionAddPartSaveBtn(ActionEvent actionEvent) throws IOException {
-        try {
-//            boolean isPartInHouse;
-//            int id = Integer.parseInt(addPartIdTxt.getText());
-//            String name = addPartNameTxt.getText();
-//            int stock = Integer.parseInt(addPartInvTxt.getText());
-//            double price = Double.parseDouble(addPartPriceTxt.getText());
-//            int max = Integer.parseInt(addPartMaxTxt.getText());
-//            int min = Integer.parseInt(addPartMinTxt.getText());
-//
-//            //condition to check which radio button was selected
-//            if(addPartInHouseRBtn.isSelected())
-//                isPartInHouse = true;
-//            else isPartInHouse = false;
-//
-//            Inventory.addPart(new Part(isPartInHouse, id, name, price, stock, max, min));
-
             stage = (Stage)((Button)actionEvent.getSource()).getScene().getWindow();
             scene = FXMLLoader.load(getClass().getResource("/warga/inventorysystem/view/MainForm.fxml"));
             stage.setScene(new Scene(scene));
             stage.show();
-
-        } catch(NumberFormatException e) {
-            Alert alert = new Alert(Alert.AlertType.ERROR);
-            alert.setTitle("Error Dialogue");
-            alert.setContentText("Please enter a valid value for each text field.");
-            alert.showAndWait();
-        }
     }
 
     public void onActionAddPartCancelBtn(ActionEvent actionEvent) throws IOException {
@@ -73,7 +49,11 @@ public class AddPartController implements Initializable {
         stage.show();
     }
 
-    public void onActionInHouseSetMachineId(ActionEvent actionEvent) {
+    public void onActionInHouseSetMachineId(ActionEvent actionEvent) throws IOException {
+        stage = (Stage)((RadioButton)actionEvent.getSource()).getScene().getWindow();
+        scene = FXMLLoader.load(getClass().getResource("/warga/inventorysystem/view/AddPartInHouse.fxml"));
+        stage.setScene(new Scene(scene));
+        stage.show();
     }
 
     public void onActionOutsourcedSetCompanyName(ActionEvent actionEvent) throws IOException {
