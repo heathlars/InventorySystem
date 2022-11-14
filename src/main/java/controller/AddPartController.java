bdeca3bf5ddc6aa3687f2448e109d8ff07ba5eff
Heather <larsen.hv@gmail.com>
2022-11-14 15:28:23 -0800

    Rename machine id/company name text field

--- src/main/java/controller/AddPartController.java
+++ src/main/java/controller/AddPartController.java
@@ -5,11 +5,11 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.RadioButton;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.stage.Stage;
+import model.InHouse;
+import model.Inventory;
+import model.Outsourced;
 
 import java.io.IOException;
 import java.net.URL;
@@ -29,7 +29,7 @@ public class AddPartController implements Initializable {
     public TextField addPartInvTxt;
     public TextField addPartPriceTxt;
     public TextField addPartMaxTxt;
-    public TextField addPartMachineIdTxt;
+    public TextField addPartOriginationTxt;
     public TextField addPartMinTxt;
     public Label addPartMachineIdLbl;
 
@@ -38,10 +38,31 @@ public class AddPartController implements Initializable {
     }
 
     public void onActionAddPartSaveBtn(ActionEvent actionEvent) throws IOException {
-            stage = (Stage)((Button)actionEvent.getSource()).getScene().getWindow();
+        try {
+            int id = Integer.parseInt(addPartIdTxt.getText());
+            String name = addPartNameTxt.getText();
+            int stock = Integer.parseInt(addPartInvTxt.getText());
+            double price = Double.parseDouble(addPartPriceTxt.getText());
+            int max = Integer.parseInt(addPartMaxTxt.getText());
+            int min = Integer.parseInt(addPartMinTxt.getText());
+            int machineId = Integer.parseInt(addPartOriginationTxt.getText());
+            String companyName = addPartOriginationTxt.getText();
+
+// issue: how to allow text field "addPartOriginationTxt" allow int or String value depending on radio button clicked
+            Inventory.addPart(new Outsourced(id, name, price, stock, min, max, companyName));
+            Inventory.addPart(new InHouse(id, name, price, stock, max, min, machineId));
+
+            stage = (Stage) ((Button) actionEvent.getSource()).getScene().getWindow();
             scene = FXMLLoader.load(getClass().getResource("/warga/inventorysystem/view/MainForm.fxml"));
             stage.setScene(new Scene(scene));
             stage.show();
+
+        } catch(NumberFormatException e) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error Dialogue");
+            alert.setContentText("Please enter a valid value type for each text field.");
+            alert.showAndWait();
+        }
     }
 
     public void onActionAddPartCancelBtn(ActionEvent actionEvent) throws IOException {
